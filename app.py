103d5df6291b17b129883fb9a8e034017bae7714
Aian <ajfuentespina@gmail.com>
2023-11-28 11:31:26 +0800

    Revert

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from contextlib import contextmanager
-from io import StringIO
-from streamlit.scriptrunner.script_run_context import SCRIPT_RUN_CONTEXT_ATTR_NAME
-from threading import current_thread
-import sys
 import streamlit as st
-
 import memory as mem
 import cache as cch
 
@@ -28,37 +22,3 @@ with st.container():
     if st.button("Start", type="primary"):
         cache = cch.create_cache(cache_blocks, cache_lines)
         memory = mem.create_memory(cache_lines, memory_blocks)
-
-@contextmanager
-def st_redirect(src, dst):
-    placeholder = st.empty()
-    output_func = getattr(placeholder, dst)
-
-    with StringIO() as buffer:
-        old_write = src.write
-
-        def new_write(b):
-            if getattr(current_thread(), SCRIPT_RUN_CONTEXT_ATTR_NAME, None):
-                buffer.write(b)
-                output_func(buffer.getvalue())
-            else:
-                old_write(b)
-
-        try:
-            src.write = new_write
-            yield
-        finally:
-            src.write = old_write
-
-@contextmanager
-def st_stdout(dst):
-    with st_redirect(sys.stdout, dst):
-        yield
-
-@contextmanager
-def st_stderr(dst):
-    with st_redirect(sys.stderr, dst):
-        yield
-
-with st_stdout("code"):
-    print("Prints as st.code()")
