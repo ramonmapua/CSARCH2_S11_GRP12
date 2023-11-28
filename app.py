3729da2eba4f5d98bcb2c7df2ee2960314fbd24a
Aian <ajfuentespina@gmail.com>
2023-11-28 10:07:54 +0800

    Testing

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+cache_blocks = 32
+cache_line = 16
+
+## streamlit page set-up
 st.set_page_config(page_title="Group 12 Cache Project", page_icon=":floppy_disk:")
 
 # Header Section
@@ -8,8 +12,6 @@ st.subheader("CSARCH2 S11 Group 12")
 
 # Project Section
 with st.container():
-    cache_blocks = 32
-    cache_line = 16
     st.write("Cache blocks = ", cache_blocks)
     st.write("Cache line = ", cache_line)
     st.write("Read policy = Non-Load Through")
