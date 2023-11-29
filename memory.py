c31a281f9de8dfc84cdf29b47a1752df23fc4447
Aian <ajfuentespina@gmail.com>
2023-11-29 11:29:56 +0800

    Display Memory Tweak

--- memory.py
+++ memory.py
@@ -34,7 +34,7 @@ def create_memory(cache_lines, memory_blocks):
             string = ""
             for block in range(memory_blocks):
                 for line in range(cache_lines):
-                    string += str(memory_queue[block][line]) + " "
+                    string += "%-3d" % str(memory_queue[block][line]) + " "
                 string += "\n"
 
             st.code(string)
